e7f710adb366446d4e59d227a5ed831a452ab5dd
yefga <yefga.tp@icloud.com>
2026-03-23 09:32:13 +0700

    feat: upgrade simu formula to v0.1.2

--- Formula/simu.rb
+++ Formula/simu.rb
@@ -1,8 +1,8 @@
 class Simu < Formula
   desc 'CLI tool to manage iOS simulators and Android emulators on macOS'
   homepage 'https://github.com/yefga/Simu'
-  url 'https://github.com/yefga/Simu/archive/refs/tags/v0.1.1.tar.gz'
-  sha256 'f35772a47b36d4f63c6e65fbbf5b6bea1420ec89c2ed63629259fe9163355580'
+  url 'https://github.com/yefga/Simu/archive/refs/tags/v0.1.2.tar.gz'
+  sha256 '816747e4fb273210f6b517d7cab8884de27f16eedccbce39643489f1607cb3c1'
   license 'MIT'
 
   depends_on 'ruby'
@@ -10,7 +10,7 @@ class Simu < Formula
   def install
     ENV['GEM_HOME'] = libexec
     system 'gem', 'build', 'simu.gemspec'
-    system 'gem', 'install', 'simu-0.1.1.gem'
+    system 'gem', 'install', 'simu-0.1.2.gem'
     bin.install libexec/'bin/simu'
     bin.env_script_all_files(libexec/'bin', GEM_HOME: ENV['GEM_HOME'])
   end
