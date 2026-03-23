ac42e51c6cf44e1c218ad5919d1c8c0901e9fbaf
yefga <yefga.tp@icloud.com>
2026-03-23 22:44:30 +0700

    feat: upgrade tap to v0.3.0 builder deployment

--- Formula/simu.rb
+++ Formula/simu.rb
@@ -1,8 +1,8 @@
 class Simu < Formula
   desc 'CLI tool to manage iOS simulators and Android emulators on macOS'
   homepage 'https://github.com/yefga/Simu'
-  url 'https://github.com/yefga/Simu/archive/refs/tags/v0.2.0.tar.gz'
-  sha256 '7760f4347bae44ced8b12820c2c6939baf048a5f22d7ddca5c385bd17adcb91b'
+  url 'https://github.com/yefga/Simu/archive/refs/tags/v0.3.0.tar.gz'
+  sha256 '030438b4ca3fbbc11ff8cbaba5da4b67d58a8aeb4bcc687002016da10c85b1ed'
   license 'MIT'
 
   depends_on 'ruby'
@@ -10,7 +10,7 @@ class Simu < Formula
   def install
     ENV['GEM_HOME'] = libexec
     system 'gem', 'build', 'simu.gemspec'
-    system 'gem', 'install', 'simu-0.2.0.gem'
+    system 'gem', 'install', 'simu-0.3.0.gem'
     bin.install libexec/'bin/simu'
     bin.env_script_all_files(libexec/'bin', GEM_HOME: ENV['GEM_HOME'])
   end
