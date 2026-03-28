ed1ae644c19389b31a7bccbd2b7f95eb7324b0e3
yefga <yefga@outlook.com>
2026-03-29 01:54:32 +0700

    feat(xunsu): update sha256 for v0.1.0

--- Formula/xunsu.rb
+++ Formula/xunsu.rb
@@ -2,7 +2,7 @@ class Xunsu < Formula
   desc 'Swift-native iOS/macOS automation tool - a lightweight fastlane alternative'
   homepage 'https://github.com/yefga/Xunsu'
   url 'https://github.com/yefga/Xunsu/archive/refs/tags/v0.1.0.tar.gz'
-  sha256 'PLACEHOLDER'
+  sha256 '7cbc299e20b5a1f15bf0b9a538fa4ef04838d4e2946b420a95cf5d97a5a04b83'
   license 'MIT'
 
   depends_on xcode: ['16.0', :build]
