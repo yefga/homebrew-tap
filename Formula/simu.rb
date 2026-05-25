b33e90f40a1c3254df9a4ad361ab9d2605c02d33
yefga <yefga@outlook.com>
2026-05-25 12:37:08 +0700

    feat: upgrade simu formula to v0.4.0

--- Formula/simu.rb
+++ Formula/simu.rb
@@ -1,21 +1,21 @@
 class Simu < Formula
-  desc 'CLI tool to manage iOS simulators and Android emulators on macOS'
-  homepage 'https://github.com/yefga/Simu'
-  url 'https://github.com/yefga/Simu/archive/refs/tags/v0.3.0.tar.gz'
-  sha256 '84efa40f52c15867741766e2ff19244d569bf1135b499c3d157886e73ffc73bc'
-  license 'MIT'
+  desc "CLI tool to manage Apple simulators and Android emulators"
+  homepage "https://github.com/yefga/Simu"
+  url "https://github.com/yefga/Simu/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "8ebdf949a8dfbf91156496c52e967c6616b15e049f40a550962e2339488caf53"
+  license "MIT"
 
-  depends_on 'ruby'
+  depends_on "ruby"
 
   def install
-    ENV['GEM_HOME'] = libexec
-    system 'gem', 'build', 'simu.gemspec'
-    system 'gem', 'install', 'simu-0.3.0.gem'
-    bin.install libexec/'bin/simu'
-    bin.env_script_all_files(libexec/'bin', GEM_HOME: ENV['GEM_HOME'])
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "simu.gemspec"
+    system "gem", "install", "simu-0.4.0.gem"
+    bin.install libexec/"bin/simu"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 
   test do
-    assert_match 'version', shell_output("#{bin}/simu version")
+    assert_match "0.4.0", shell_output("#{bin}/simu version")
   end
 end
