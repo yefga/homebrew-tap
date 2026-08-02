c80d124061b08bd18714d038a669451ea7891923
yefga <yefga@outlook.com>
2026-08-03 00:48:11 +0700

    chore: update switzy cask to v1.1.0

--- Casks/switzy.rb
+++ Casks/switzy.rb
@@ -1,11 +1,13 @@
 cask "switzy" do
-  version "1.0.1"
-  sha256 "c03a770bc0f1fe0bd0edb4142718a0376725dd5e8d897aef747480baaf2547b8"
+  version "1.1.0"
+  sha256 "5f11bf8b8557cf863c142e6cb0d780168e518b0dcbaabbaad084cb4655cdabf1"
 
   url "https://github.com/yefga/Switzy/releases/download/v#{version}/Switzy-v#{version}.dmg"
   name "Switzy"
   desc "Git Identity Manager"
   homepage "https://github.com/yefga/Switzy"
+
+  depends_on macos: :ventura
 
   app "Switzy.app"
 
